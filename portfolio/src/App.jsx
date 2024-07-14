891f7fdcaa921e3f92392e8bf78882eec497c2b7
Tamanna Sheikh <tamannasheikh2304@gmail.com>
2024-07-14 18:13:21 +0000

    adding glow effect

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -8,7 +8,12 @@ function App() {
 
   return (
     <>
-     My Portfolio Website
+     <div className="heading-box">
+      <div className="text-pink-700 font-bold text-5xl pb-5">WELCOME TO MY PORTFOLIO WEBSITE !!</div>
+      <div className="glow-box">
+      <div className="text-pink-700 font-bold text-3xl" >I am Tamanna Sheikh</div>
+        </div>
+     </div>
     </>
   )
 }
