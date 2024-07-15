d331ca9e1bdf011328e10f152cb768a2d65a66fb
Tamanna Sheikh <tamannasheikh2304@gmail.com>
2024-07-15 18:35:50 +0000

    adding about me

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import image from './image.png';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,10 +8,18 @@ function App() {
   return (
     <>
      <div className="heading-box">
-      <div className="text-pink-700 font-bold text-5xl pb-5">WELCOME TO MY PORTFOLIO WEBSITE !!</div>
-      <div className="glow-box">
-      <div className="text-pink-700 font-bold text-3xl" >I am Tamanna Sheikh</div>
-        </div>
+      <div className="text-white text-shadow-md font-bold text-5xl pb-5">WELCOME TO MY PORTFOLIO WEBSITE !!</div>
+      <div className="text-white font-bold text-3xl mb-10">I am Tamanna Sheikh</div>
+     </div>
+
+     <div className="flex">
+     <img className="max-w-auto max-h-auto justify-start"src={image} alt='Tammy'/>
+     <p className="">My name is Tamanna Sheikh. I am currently pursuing my B.Tech in Artificial Intelligence and Machine Learning.
+      I completed my schooling from Carmel Convent School.Throughout my academic career,I consistently achieved top ranks in my school and college.
+      My aggregate CGPA is 8.89.My interests lie in Programming and machine learning.I have strong knowledge about Data Structures and Algorithms.My greatest strenghts are my perseverance and sense of righteousness.
+     </p>
+      
+
      </div>
     </>
   )
