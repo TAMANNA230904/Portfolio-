97135648c883442d69fed70a8b6900cb46ceccd4
Tamanna Sheikh <tamannasheikh2304@gmail.com>
2024-07-16 17:56:20 +0000

    about me

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -12,13 +12,16 @@ function App() {
       <div className="text-white font-bold text-3xl mb-10">I am Tamanna Sheikh</div>
      </div>
 
-     <div className="flex">
-     <img className="max-w-auto max-h-auto justify-start"src={image} alt='Tammy'/>
-     <p className="">My name is Tamanna Sheikh. I am currently pursuing my B.Tech in Artificial Intelligence and Machine Learning.
+     <div className="grid grid-cols-2 gap-4">
+     <img className="max-w-auto max-h-auto justify-start"src='' alt='Tammy'/>
+     <p className="text-xl indent-0 border-2 border-pink-700 p-2">My name is Tamanna Sheikh. I am currently pursuing my B.Tech in Artificial Intelligence and Machine Learning.
       I completed my schooling from Carmel Convent School.Throughout my academic career,I consistently achieved top ranks in my school and college.
-      My aggregate CGPA is 8.89.My interests lie in Programming and machine learning.I have strong knowledge about Data Structures and Algorithms.My greatest strenghts are my perseverance and sense of righteousness.
+      My aggregate CGPA is 8.89.My interests lie in Programming and machine learning.I have strong knowledge about Data Structures and Algorithms.
+      I have worked on various machine learning projects such as House Price Prediction,Handwritten Digit Recognition.
+      Skils included in my tech stack are C++,HTML,CSS,React,Javascript,SQL.I always try to learn new things and adaptive to new technologies.
+      My greatest strenghts are my perseverance and sense of righteousness.I love singing and have been learning to play guitar these days.
      </p>
-      
+     
 
      </div>
     </>
