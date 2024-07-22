a4bd0b554c8be4caebd7bc7676da077b8ba91fb0
Tamanna Sheikh <tamannasheikh2304@gmail.com>
2024-07-22 18:13:58 +0000

    skills

--- portfolio/src/Skills.jsx
+++ portfolio/src/Skills.jsx
@@ -3,12 +3,13 @@ import './App.css'
 
 const Skills=()=>{
     return (
-   <div className="mt-9 p-9">
-    <h3>Technical Skills</h3>
+   <div className="grid grid-cols-2 gap-4 m-9 justify-items-center  items-center">
+    <h3 className="m-0 p-0 font-bold text-4xl w-2/3 ">Technical Skills</h3>
+    <div className="w-40">
    <div className="">
     <div className="text-white-700 m-5">C++</div>
     <div className="h-4 relative bg-neutral-500 rounded-lg">
-        <div className=" absolute left-0 h-4 w-4/5 bg-red-400 "></div>
+        <div className=" absolute left-0 h-4 w-4/5 bg-red-400 rounded-lg "></div>
     </div>
    </div>
 
@@ -40,7 +41,7 @@ const Skills=()=>{
         <div className="skill-per" ></div>
     </div>
    </div>
-
+   </div>
 
    </div>
     )
