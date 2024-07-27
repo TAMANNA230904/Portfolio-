861eab9372600a611938724f7e6d1dd911faf213
Tamanna Sheikh <tamannasheikh2304@gmail.com>
2024-07-27 17:57:08 +0000

    code profiles

--- portfolio/src/Skills.jsx
+++ portfolio/src/Skills.jsx
@@ -2,8 +2,18 @@ import { useState } from 'react'
 import './App.css'
 
 const Skills=()=>{
+    const onButtonClick = () => {
+        const pdfUrl = "Resume.pdf";
+        const link = document.createElement("a");
+        link.href = pdfUrl;
+        link.download = "document.pdf"; // specify the filename
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return (
-   <div>
+   <div className="flex">
    {/* Technical Skills with bar */}
    <div className="flex-row m-9  justify-items-center  items-center w-2/3">
     <h3 className="mb-7 p-0 font-bold text-4xl  ">Technical Skills</h3>
@@ -54,7 +64,17 @@ const Skills=()=>{
    </div>
    </div>
  {/* Profiles section */}
+    
+        <a href="https://leetcode.com/u/tamannasheikh2304/">
+        <img className="w-12 h-12 rounded-3xl"src="https://user-images.githubusercontent.com/36547915/97088991-45da5d00-1652-11eb-900f-80d106540f4f.png" alt="leetcode" />
+        </a>
 
+        <a href="https://www.geeksforgeeks.org/user/tamannashi5kn/?utm_source=geeksforgeeks&utm_medium=my_profile&utm_campaign=auth_user">
+        <img className="w-12 h-12 rounded-3xl"src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/gfg_200x200-min.png" alt="leetcode" />
+        </a>
+
+        <button onClick={onButtonClick} className="">Resume</button>
+    
    </div>
     )
 };
